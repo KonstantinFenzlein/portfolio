769be85d029e9379300b7bb554a5111d4069f4a4
KonstantinFenzlein <kfenzlein@gmail.com>
2026-02-04 11:08:42 +0100

    framework of all sections

--- src/app/pages/home/home.ts
+++ src/app/pages/home/home.ts
@@ -1,11 +1,27 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+
+import { Hero } from './hero/hero';
+import { AboutMe } from './about-me/about-me';
+import { Skills } from './skills/skills';
+import { Portfolio } from './portfolio/portfolio';
+import { Friends } from './friends/friends';
+import { Contact } from './contact/contact';
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  standalone: true,
+  imports: [
+    CommonModule,
+    Hero,
+    AboutMe,
+    Skills,
+    Portfolio,
+    Friends,
+    Contact
+  ],
   templateUrl: './home.html',
-  styleUrl: './home.scss',
+  styleUrl: './home.scss'
 })
-export class HomeComponent {
-
-}
+export class HomeComponent {}
