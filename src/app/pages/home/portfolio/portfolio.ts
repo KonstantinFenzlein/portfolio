b8d8d09c0802bba3c704aa6dab5b6a3f7c33d0bd
KonstantinFenzlein <kfenzlein@googlemail.com>
2026-07-12 23:12:49 +0200

    update github-link

--- src/app/pages/home/portfolio/portfolio.ts
+++ src/app/pages/home/portfolio/portfolio.ts
@@ -26,7 +26,7 @@ export class PortfolioComponent {
       techStack: 'JavaScript | HTML | CSS',
       descriptionDE: 'Springen, rennen und werfen – ein objektorientiertes Spiel. Hilf Pepe, Münzen und Tabasco-Salsa zu finden, um die verrückte Henne zu besiegen.',
       descriptionEN: 'Jump, run and throw – an object-oriented game. Help Pepe find coins and tabasco sauce to defeat the crazy chicken.',
-      githubLink: 'https://github.com/KonstantinFenzlein/pollo-loco'
+      githubLink: 'https://github.com/KonstantinFenzlein/el_pollo_loco'
     }
   ];
 
